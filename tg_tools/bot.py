a9966033d44cf0acbfaae7e55089a3ff0ea56fd4
KonuhovAND <andreykonuhov8@gmail.com>
2025-10-18 21:54:27 +0300

    Enhance tg_bot functionality with new command handlers and improved user interaction

--- tg_tools/bot.py
+++ tg_tools/bot.py
@@ -2,18 +2,20 @@ import asyncio
 import os
 import signal
 from tg_tools.sm import dev_profile, scenario
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler, CallbackQueryHandler
 from telegram.error import TelegramError
 from tools.run_parser import runner
 from time import time
+
+
 
 DATA = 1
 class tg_bot:
     def __init__(self, token_bot):
         self.token_bot = token_bot
         self.application = None
-
+    # Tools commands
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(dev_profile)
 
@@ -25,26 +27,53 @@ class tg_bot:
                 await update.message.reply_text(link)
         if option == "exit":
             os.kill(os.getpid(), signal.SIGINT)
+    async def info(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        info_text = """
+🤖 Available Commands:
 
-    # Entry point for JSON parsing
-    async def parse_to_json(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text("Enter amount of days to parse and leauge (days league)\nNHL, KHL, VHL, MHL, all:")
-        context.user_data['mode'] = 'json'
-        return DATA
+/start - Show main menu
+/parse_json - Parse and get JSON file
+/parse_db - Parse and get DB file
+/parse_json_and_db - Parse and get both files
+/info - Show this help
+/cancel - Cancel current operation
+        """
+        await update.message.reply_text(info_text)
 
-    # Entry point for DB parsing
-    async def parse_to_db(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text("Enter amount of days to parse and leauge (days league)\nNHL, KHL, VHL, MHL, all:")
-        context.user_data['mode'] = 'db'
-        return DATA
-
-    # Entry point for both JSON and DB
-    async def parse_to_json_and_db(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text("Enter amount of days to parse and leauge (days league)\nNHL, KHL, VHL, MHL, all:")
-        context.user_data['mode'] = 'both'
-        return DATA
-
+    async def echo(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text(update.message.text)
+        
+    async def button_handler(update: Update, context):
+        query = update.callback_query
+        await query.answer()  # Remove loading animation
+        await query.edit_message_text(f"You selected: {query.data}")
     
+    async def cancel(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("Operation cancelled")
+        return ConversationHandler.END
+    
+    async def post_init(self, application: Application):
+            await application.bot.set_my_commands([
+                ("start", "Show main menu"),
+                ("parse", "Parse to JSON/DB/both"),
+                ("info", "Show commands"),
+                ("cancel", "Cancel operation"),
+            ])
+        
+    # Parsing commands
+    async def parse(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("Enter amount of days to parse:")
+        keyboard = [
+        [InlineKeyboardButton("json", callback_data='json')],
+        [InlineKeyboardButton(".db file", callback_data='db')],
+        [InlineKeyboardButton("both", callback_data='both')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text('Choose an option:', reply_markup=reply_markup)
+        context.user_data['mode'] =  InlineKeyboardButton.callback_data()
+        
+        return DATA
+
 
     async def run_parser_and_send_file(self, chat_id, mode, days,league):
         try:
@@ -100,21 +129,18 @@ class tg_bot:
                 text="❌ Failed to send files."
             )
 
-    async def info(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        info_text = """
-🤖 Available Commands:
-
-/start - Show main menu
-/parse_json - Parse and get JSON file
-/parse_db - Parse and get DB file
-/parse_json_and_db - Parse and get both files
-/info - Show this help
-/cancel - Cancel current operation
-        """
-        await update.message.reply_text(info_text)
-
-    async def echo(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text(update.message.text)
+    
+    
+    async def hockey_options(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        keyboard = [
+        [InlineKeyboardButton("NHL", callback_data='nhl')],
+        [InlineKeyboardButton("KHL", callback_data='khl')],
+        [InlineKeyboardButton("VHL", callback_data='vhl')],
+        [InlineKeyboardButton("MHL", callback_data='mhl')],
+        [InlineKeyboardButton("All", callback_data='all')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text('Choose an option:', reply_markup=reply_markup)
         
         
     async def receive_data(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -123,12 +149,13 @@ class tg_bot:
             days = data.split(" ")[0].strip()
             league = data.split(" ")[1].strip()
         except Exception as exc:
-            await update.message.reply_text("Please enter a valid number of days and league (days league)\nNHL, KHL, VHL, MHL, all:")
+            await update.message.reply_text("Please enter a valid number of days:")
             return DATA
         # Validate input
-        if not days.isdigit() or int(days) <= 0 or league not in ["nhl","khl","vhl","mhl","all"]:
-            await update.message.reply_text("Please enter a valid number of days and league (days league)\nNHL, KHL, VHL, MHL, all:")
+        if not days.isdigit() or int(days) <= 0:
+            await update.message.reply_text("Please enter a valid number of days:")
             return DATA
+        self.hockey_options(update, context)
         
         chat_id = update.message.chat_id
         mode = context.user_data.get('mode')
@@ -139,19 +166,7 @@ class tg_bot:
         
         return ConversationHandler.END
 
-    async def cancel(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text("Operation cancelled")
-        return ConversationHandler.END
-    
-    async def post_init(self, application: Application):
-            await application.bot.set_my_commands([
-                ("start", "Show main menu"),
-                ("parse_json", "Parse to JSON"),
-                ("parse_db", "Parse to DB"),
-                ("parse_json_and_db", "Parse to both"),
-                ("info", "Show commands"),
-                ("cancel", "Cancel operation"),
-            ])
+   
 
     def main(self):
         self.application = Application.builder().token(self.token_bot).build()
@@ -159,9 +174,7 @@ class tg_bot:
         # Single ConversationHandler with 3 entry points
         conv_handler = ConversationHandler(
             entry_points=[
-                CommandHandler("parse_json", self.parse_to_json),
-                CommandHandler("parse_db", self.parse_to_db),
-                CommandHandler("parse_json_and_db", self.parse_to_json_and_db)
+                CommandHandler("parse", self.parse),
             ],
             states={
                 DATA: [MessageHandler(filters.TEXT & ~filters.COMMAND, self.receive_data)]
@@ -173,6 +186,7 @@ class tg_bot:
         # Add handlers
         self.application.add_handler(CommandHandler("start", self.start))
         self.application.add_handler(conv_handler)
+        self.application.add_handler(CallbackQueryHandler(self.button_handler))
         self.application.add_handler(CommandHandler("info", self.info))
         self.application.add_handler(
             CommandHandler(["help", "projects", "showskils", "contact", "exit"], self.options)
